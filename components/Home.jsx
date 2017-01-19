228fd731d8aead13797bdb0eeea9a53a42bc8321
Maia Kamyab <mahtakamyab@gmail.com>
2017-01-19 14:42:09 -0800

    add react router

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class Home extends Component {
   render() {
-    return <h1>Hello</h1>
+    return <h1><Link to="calendar">Click here to go to calendar</Link></h1>
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import Home from './components/Home.jsx';
+import Calendar from './components/Calendar.jsx';
 
-ReactDOM.render(<Home/>, document.getElementById('home'));
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route name="home" path="/" component={Home} />
+        <Route name="calendar" path="/calendar" component={Calendar} />
+    </Router>,
+    document.getElementById('home')
+);
