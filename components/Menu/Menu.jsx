db5e69e7dd8dac4377080ac14db437f0587cacf2
Maia Kamyab <mahtakamyab@gmail.com>
2017-01-22 18:56:38 -0800

    add watch event for menu resizing

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
 import css from './menu.css';
 import {isMobile} from '../utility.js';
 import MobileMenu from './MobileMenu.jsx';
+import {debounce} from 'underscore';
 
-export default function Menu() {
-  return isMobile() ? <MobileMenu /> : desktopMenu();
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: isMobile()
+    };
+    this.toggleDevice = this.toggleDevice.bind(this);
+  }
+
+  toggleDevice() {
+    this.setState({isMobile: isMobile()});
+  }
+
+  componentDidMount() {
+    const debounceToggle = debounce(this.toggleDevice, 500);
+    window.addEventListener('resize', debounceToggle);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize');
+  }
+
+  render() {
+    return this.state.isMobile ? <MobileMenu /> : desktopMenu();
+  }
 }
 
 function desktopMenu() {
@@ -13,12 +37,12 @@ function desktopMenu() {
     <div>
       <div className={css.placeholder}></div>
       <div className={css.wrapper}>
-        <div className={css.menuItem}><Link to="/">Home</Link></div>
+        <Link to="/" className={css.menuItem}>Home</Link>
         <div className={css.itemWrapper}>
-          <div className={css.menuItem}><Link to="about">About</Link></div>
-          <div className={css.menuItem}><Link to="calendar">Calendar</Link></div>
-          <div className={css.menuItem}><Link to="members">Member Resources</Link></div>
-          <div className={css.menuItem}><Link to="vr">VR Division</Link></div>
+          <Link to="about" className={css.menuItem}>About</Link>
+          <Link to="calendar" className={css.menuItem}>Calendar</Link>
+          <Link to="members" className={css.menuItem}>Member Resources</Link>
+          <Link to="vr" className={css.menuItem}>VR Division</Link>
         </div>
       </div>
     </div>
